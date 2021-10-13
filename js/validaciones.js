81640c0b71fcf60f1da42f46fdc9e2d2eab989a4
maurisantiagol <msantiagod1901@alumno.ipn.mx>
2021-10-13 16:50:39 -0500

    validaciones sin implementacion

--- js/validaciones.js
+++ js/validaciones.js
@@ -2,7 +2,14 @@ function validarEmail(email) {
   var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   return regex.test(email) ? true : false;
 }
-
+function validarNombres(Nombre) {
+  var regex = /^[A-Z]+$/i;
+  return regex.test(Nombre) ? true : false;
+}
+function validarContraseñas(Contraseña) {
+  var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+  return regex.test(Contraseña) ? true : false;
+}
 
 
 function CalcularEdad(fecha_nacimiento) {
@@ -26,6 +33,9 @@ function Validar(event) {
     alert("Eres menor de edad ");
   }
   var email = validarEmail(document.getElementById("inputemail").value);
+  var nombre = validarNombres(document.getElementById("inputNombre").value);
+  var contraseña = validarContraseñas(document.getElementById("inputContraseña1").value);
+  var confirmacion = validarContraseñas(document.getElementById("inputContraseña2").value);
 
   //colocar lo que va a hacer en caso de que las validaciones sean correctas o incorrectas
 
@@ -33,6 +43,20 @@ function Validar(event) {
     alert("La dirección de email " + document.getElementById("inputemail").value + " es correcta.");
   } else {
     alert("La dirección de email " + document.getElementById("inputemail").value + " es incorrecta.");
+  }
+  if (nombre == true) {
+    alert("El nombre " + document.getElementById("inputNombre").value + " es correcto.");
+  } else {
+    alert("El nombre " + document.getElementById("inputNombre").value + " es incorrecto.");
+  }
+  alert(document.getElementById("inputContraseña1").value);
+  alert(document.getElementById("inputContraseña2").value);
+  alert(contraseña);
+  alert(confirmacion);
+  if (contraseña == true && confirmacion == true && document.getElementById("inputContraseña1").value==document.getElementById("inputContraseña2").value) {
+    alert("Las contraseñas "  + " son correctas.");
+  } else {
+    alert("Las contraseñas "+ " son incorrectas.");
   }
 
 
