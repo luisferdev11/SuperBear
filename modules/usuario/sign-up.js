8d3f821e65f85d91489c8d57d78cee913b55d75c
maurisantiagol <msantiagod1901@alumno.ipn.mx>
2021-11-07 20:19:54 -0600

    Registro finalizado sin pruebas suficientes

--- modules/usuario/sign-up.js
+++ modules/usuario/sign-up.js
@@ -11,40 +11,33 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views"));
 
 router.use(passport.initialize());
-passport.use(
-    new passportlocal(async function (req, done) {
-        /*console.log("hola peruano");
+
+
+router.post(
+    "/sign-up", async (req, res) => {
         const { email } = req.body;
         const { nombre } = req.body;
         const { fecha } = req.body;
-        const { contraseña2 } = req.body;
+        const { Password2 } = req.body;
         const { SelectAlcaldia } = req.body;
         const { genero } = req.body;
-        console.log(email, nombre, fecha, contraseña2, SelectAlcaldia, genero);
-        let newUser = {
-            email,
+
+        let newUser = [email,
             nombre,
             fecha,
-            contraseña2,
+            Password2,
             SelectAlcaldia,
-            genero,
-        };
-        // Saving in the Database
-        const result = await pool.query(
-            "INSERT INTO musuario (cor_usu, nom_usu, fec_nac, con_usu, id_alc, id_sex) VALUES (?, ?, ? , ?, ?, ?)",
-            newUser
-        );*/
-        return done(null);
-        done(null, false);
-    })
-);
-router.post(
-    "/sign-up",
-    passport.authenticate("local", {
-        successRedirect: "/login",
-        failureRedirect: "/",
-    })
-);
+            genero];
+        try {
+
+        await pool.query("INSERT INTO musuario (cor_usu, nom_usu, fec_nac, con_usu, id_alc, id_sex) VALUES (?,?,?,?,?,?)", newUser);
+            res.render("iniciarSesion");
+        } catch (err) {
+            res.render("error");
+            console.log(err);
+        }
+    });
+
 router.get("/sign-up", (req, res) => {
     res.render("registro");
 });
