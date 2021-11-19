ac3d15663a3139123ad64b15d5af38a7897fd9ed
thepresidentm <miguelanva9@gmail.com>
2021-11-19 01:34:29 -0600

    crear noticia sin id admin ni validacion

--- modules/noticias/noticias.js
+++ modules/noticias/noticias.js
@@ -14,6 +14,21 @@ module.exports = {
     async getNoticiasAdmin(req, res){
         let noticias = await pool.query('SELECT * FROM dnoticias');
         res.render('admin-consultarNoticias', {noticias: noticias});
-    }
+    },
 
+    async setNoticia(req, res){
+        let { titulo } = await req.body;
+        let { contenido } = await req.body;
+        // Falta obtener el id del admin
+        let id_Adm = 1;
+        // Falta validacion
+        let validacion = true;
+        if (validacion == true){
+            pool.query(
+                'insert into dnoticias (tit_not, cont_not, fec_not, id_Adm) values ("' + titulo + '", "' + contenido + '", NOW(), ' + id_Adm + ');'
+            );
+            res.redirect('/admin-consultarNoticias');
+        }
+        
+    }
 }
--- modules/noticias/routes.js
+++ modules/noticias/routes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../auth/auth");
+const { setNoticia } = require("./noticias");
 const controller = require('./noticias');
 
 router.get("/consultarNoticias-usuario", auth.isAuthenticated, controller.getNoticias);
 
+// Admin
+// Se debe de cambiar por adminIsAuth
 router.get("/admin-consultarNoticias", auth.isAuthenticated, controller.getNoticiasAdmin);
 
+router.get("/crearNoticia", auth.isAuthenticated, (req, res) => {
+    res.render("admin-crearNoticia");
+});
+
+router.post("/crearNoticia", auth.isAuthenticated, setNoticia);
 module.exports = router;
