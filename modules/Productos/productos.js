3803b7bb40d7a63f23d37c2c46edd84d0973c155
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2021-11-20 16:22:09 -0600

    Problemas al visualizar productos

--- modules/Productos/productos.js
+++ modules/Productos/productos.js
@@ -1,72 +1,80 @@
-const jwt = require("jsonwebtoken");
-const bcryptjs = require("bcryptjs");
 const pool = require("../../database");
-const { promisify } = require("util");
-const { env } = require("../../credenciales");
 
 async function getAllMarca() {
     const marca = await pool.query(
         `SELECT * FROM cmarca`
     );
-    return marca[0].marca.trim();
+    return marca;
 }
 
 async function getAllDepa() {
     const depa = await pool.query(
         `SELECT * FROM CDepartamento`
     );
-    return depa[0].depa.trim();
+    return depa;
 }
 async function getAllUni() {
     const unidad = await pool.query(
         `SELECT * FROM CUnidad`
     );
-    return unidad[0].unidad.trim();
+    return unidad;
 }
 
 async function getAllSuper() {
     const Sup = await pool.query(
         `SELECT * FROM CSupermercado`
     );
-    return Sup[0].Sup.trim();
+    return Sup;
 }
 
 module.exports = {
-    async CrearProducto(req, res) {
-        const id = req.user.id_usu;
-        const idl = req.params;
+    async CrearProducto(req, res) {       
         const { nombre } = req.body;
-        const {Marca} = req.body;
-        const {Super} = req.body;
-        const {Depa} = req.body;
-        const {cantidad} = req.body;
-        const {unidad} = req.body;
-        const {precio} = req.body;
-        const {Notas} = req.body;
-
-        let newP = [nombre, Marca, Super, Depa, cantidad, unidad, precio, Notas];
+        console.log(nombre);
+        const { Marca } = req.body;
+        console.log(Marca);
+        const { supermercado } = req.body;
+        console.log(supermercado);
+        const { Depa } = req.body;
+        console.log(Depa);
+        const { Cant } = req.body;
+        console.log(Cant);
+        const {Unidad} = req.body;
+        console.log(Unidad);
+        const {Precio} = req.body;
+        console.log(Precio);
+        const {Anotaciones} = req.body;
+        console.log(Anotaciones);
+        const {id_lis} = req.body;
+        console.log(id_lis);
+        
         try {
             const id_lista = await pool.query(
-                "select id_eli from ELista where id_lis = ?",
-                idl
+                "select id_eli from ELista where id_lst = ?",
+                [id_lis]
             );
+            console.log(id_lista);
             await pool.query(
-                "INSERT INTO DProducto (id_eli, nom_pro, id_mar, id_sup, id_dep, id_uni, can_pro, precio_pro, notas_pro, id_tip, id_esp) VALUES (?,?,?,?,?,?,?,?,?,?,1)",
-                [id_lista, newP]
+                "INSERT INTO DProducto (id_eli, nom_pro, id_mar, id_sup, id_dep, can_pro, id_uni, precio_pro, notas_pro, id_tip, id_esProd) VALUES (?,?,?,?,?,?,?,?,?,2,1)",
+                [id_lista[0].id_eli, nombre,parseInt(Marca), parseInt(supermercado), parseInt(Depa), parseInt(Cant), parseInt(Unidad), parseInt(Precio), Anotaciones]
             );
-            res.render("consultarProductosDeLista");
+            res.redirect("/ConsultarProductos/"+id_lis);
         } catch (err) {
             res.render("error");
             console.log(err);
         }
     },async ConsultarCatalogo (req, res){
         try {
-        const id = req.user.id_usu;
         const ideli = req.params;
         const Marca = await getAllMarca();
         const Depa = await getAllDepa();
         const Uni = await getAllUni();
         const Super = await getAllSuper();
+        console.log(ideli);
+        console.log(Marca);
+        console.log(Depa);
+        console.log(Uni);
+        console.log(Super);
         res.render("agregarProductoALista", { 
             id: ideli,
             Marca: Marca,
@@ -82,17 +90,18 @@ module.exports = {
     async ConsultarProductos (req, res){
         try {
         const idl = req.params.id_lis;
+        const name = await pool.query(
+            "select nom_lis from mlista where id_lis = ?",
+            [idl]
+        );
         const eli = await pool.query(
-            "select id_eli from elista where id_lst = ?",
+            "select * from elista where id_lst = ?",
             [idl]
         );
-        const grupo = await pool.query(
-            "select id_grp from elista where id_lst = ?",
-            [idl]
-        );
+        const grupo = eli[0].id_grp;
         var id_e = [];
             for (let i = 0; i < eli.length; i++) {
-                const miembro = eli[i].id_lst;
+                const miembro = eli[i].id_eli;
                 id_e.push(miembro);
             }            
             var productos = [];
@@ -105,12 +114,16 @@ module.exports = {
                 productos.push(list[0]);
             }
 
-            console.log(JSON.stringify(listas));
+            console.log(JSON.stringify(productos));
+            console.log(idl);
+            console.log(grupo);
+            console.log(name);
 
-            res.render("consultarListasDeGrupo", {
+            res.render("consultarProductosDeLista", {
                 productos: productos,
-                idlista: idl,
-                grupo: grupo
+                idLista: idl,
+                grupo: grupo,
+                name: name
             });
             
         } catch (err) {
