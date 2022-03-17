ba86020b7ba008d1e800c8a36d5773deeb4bb4b9
thepresidentm <miguelanva9@gmail.com>
2022-03-17 11:40:17 -0600

    responsive

--- views/js/barraNavegacion.js
+++ views/js/barraNavegacion.js
@@ -1,14 +1,23 @@
 var nav = document.getElementsByTagName("nav")[0];
 var prevScrollpos = window.pageYOffset;
 
-// Te quiero mucho w3 schools
-window.onscroll = function() {
+
+// Si es un movil, cambia la barra de navegacion
+if(SmartPhone.isAny()){
+
     nav.style.position = "fixed";
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        nav.style.bottom = "0";
-    } else {
-        nav.style.bottom = "-50px";
+    nav.style.bottom = "0";
+    // Te quiero mucho w3 schools
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            nav.style.bottom = "0";
+        } else {
+            nav.style.bottom = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
+    
 }
+
+
