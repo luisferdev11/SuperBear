569a34841ece2f45a55ef96049cc533763da0098
thepresidentm <miguelanva9@gmail.com>
2022-05-21 17:40:39 -0500

    mensaje de error base de datos

--- database.js
+++ database.js
@@ -4,6 +4,8 @@ const { database } = require('./credenciales');
 const pool = mysql.createPool(database);
 pool.getConnection((err, connection) => {
     if (err) {
+        console.log('Error base de datos:')
+        console.log(err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
             console.error('Se cerró la conexión a la base de datos.');
         }
