27bec2b3d5ff4995902f3fe78d294e4648d5a0f3
thepresidentm <miguelanva9@gmail.com>
2022-05-21 22:12:24 -0500

    rutas rutinas

---
+++ modules/rutinas/routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("./rutinas");
+
+
+// Crear Rutina
+router.get(
+    "/crearRutina/:lista/:grupo",
+    controller.consultarNombre
+);
+
+router.post(
+    "/crearRutina/:lista/:grupo",
+    controller.crearRutina
+)
+
+router.post(
+    "/crearRutina/:lista/:grupo",
+);
+
+router.get("/consultarRutinas", (req, res) => {
+    res.render("consultarRutinas");
+});
+
+router.get("/editarRutinas", (req, res) =>{
+    res.render("editarRutinas");
+});
+
+module.exports = router;
