877ef3c244b78b2cb8ada9bc105181521d8f7fd2
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2022-05-31 22:25:08 -0500

    error rutinas

--- modules/rutinas/routes.js
+++ modules/rutinas/routes.js
@@ -1,39 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./rutinas");
-
+const auth = require("../auth/auth");
 
 // Crear Rutina
 router.get(
     "/crearRutina/:lista/:grupo",
+    auth.isAuthenticated,
     controller.consultarNombre
 );
 
 router.post(
     "/crearRutina/:lista/:grupo",
+    auth.isAuthenticated,
     controller.crearRutina
-)
+);
 
 // Consultar Rutina
 router.get(
     "/consultarRutinas/:grupo",
+    auth.isAuthenticated,
     controller.consultarRutinas
 );
 
 // Borrar Rutina
 router.get(
     "/borrarRutina/:rutina/:grupo",
+    auth.isAuthenticated,
     controller.borrarRutina
 );
 
 // Editar Rutina
 router.get(
     "/editarRutina/:rutina/:grupo",
+    auth.isAuthenticated,
     controller.getEditarRutina
 );
 
 router.post(
     "/editarRutina/:rutina/:grupo",
+    auth.isAuthenticated,
     controller.editarRutina
 );
 
