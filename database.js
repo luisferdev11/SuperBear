d485aac37fd19ed2f054a56c4a1044a33a52bb2e
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2022-05-31 23:03:19 -0500

    metodo bd

--- database.js
+++ database.js
@@ -1,23 +1,25 @@
-const mysql = require('mysql');
-const { promisify }= require('util');
-const { database } = require('./credenciales');
+const mysql = require("mysql");
+const { promisify } = require("util");
+const { database } = require("./credenciales");
 const pool = mysql.createPool(database);
 pool.getConnection((err, connection) => {
     if (err) {
-        console.log('Error base de datos:')
+        console.log("Error base de datos:");
         console.log(err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Se cerró la conexión a la base de datos.');
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            console.error("Se cerró la conexión a la base de datos.");
         }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('La base de datos tiene muchas conexiones');
+        if (err.code === "ER_CON_COUNT_ERROR") {
+            console.error("La base de datos tiene muchas conexiones");
         }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Se rechazó la conexión a la base de datos');
+        if (err.code === "ECONNREFUSED") {
+            console.error("Se rechazó la conexión a la base de datos");
         }
     }
-    if (connection) connection.release();
-    console.log('DB está conectada');
+    if (connection) {
+        connection.release();
+        console.log("DB está conectada");
+    }
 
     return;
 });
