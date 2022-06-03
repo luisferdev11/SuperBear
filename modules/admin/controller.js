169d8d053bca8efb4ca6e6d4eabee1808b1ee3c7
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2022-06-02 21:43:42 -0500

    cambios suficientes en dashboard de admin

--- modules/admin/controller.js
+++ modules/admin/controller.js
@@ -19,7 +19,7 @@ module.exports = {
                 newProduct
             );
 
-            res.redirect('/check-default-products');
+            res.redirect("/check-default-products");
         } catch (err) {
             res.redirect("/error");
             console.log(err);
@@ -42,18 +42,24 @@ module.exports = {
         }
     },
     async stadistics(req, res) {
-        const listas = await pool.query(
-            "SELECT id_lis FROM mlista"
-        );
-        const grupos = await pool.query(
-            "SELECT id_grp FROM mgrupo"
-        );
-        const usuarios = await pool.query(
-            "SELECT id_usu FROM musuario"
-        );
-console.log(listas.length,grupos.length,usuarios.length);
-        res.render("admin-index", { numListas:listas.length, numGrupos:grupos.length, numUsuarios:usuarios.length});
+        let [listas, grupos, usuarios, sup, noti, marc] = await Promise.all([
+            pool.query("SELECT COUNT(id_lis) AS rslt FROM mlista"),
+            pool.query("SELECT COUNT(id_grp) AS rslt FROM mgrupo"),
+            pool.query("SELECT COUNT(id_usu) AS rslt FROM musuario"),
+            pool.query("SELECT COUNT(id_sup) AS rslt FROM csupermercado"),
+            pool.query("SELECT COUNT(idDNoticias) AS rslt FROM dnoticias"),
+            pool.query("SELECT COUNT(id_mar) AS rslt FROM cmarca"),
+        ]);
 
+        console.log(listas[0].rslt, grupos[0].rslt, usuarios[0].rslt);
+        res.render("admin-index", {
+            numListas: listas[0].rslt,
+            numGrupos: grupos[0].rslt,
+            numUsuarios: usuarios[0].rslt,
+            numSup: sup[0].rslt,
+            numNoti: noti[0].rslt,
+            numMarc: marc[0].rslt,
+        });
     },
     async delete(req, res) {
         //falta hacer un for que nos de todos los ids de los que tienen admin y un if(si arridpriv[i]==req.user.id_usu{})
